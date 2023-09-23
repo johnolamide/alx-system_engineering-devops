0e689531395306f3642cb83a1b56f6fe1f0aa569
Olamide Alokan <alokanolamide@gmail.com>
2023-09-23 20:21:19 +0100

    [ADD] 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# This Puppet manifest configures ssh configuration file
+
+file { '~/.ssh/config',
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "
+    Host *
+      IdentifyFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
