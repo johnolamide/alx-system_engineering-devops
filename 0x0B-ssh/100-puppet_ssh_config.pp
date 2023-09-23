ad2cea8a366b5dfac0e7cfcc1cdf236db6d2f4df
Olamide Alokan <alokanolamide@gmail.com>
2023-09-23 20:33:37 +0100

    [ADD] 2-execute_a_command.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,14 @@
 # This Puppet manifest configures ssh configuration file
 
-#file { '~/.ssh/config':
-#  ensure  => file,
-#  owner   => 'ubuntu',
-#  group   => 'ubuntu',
-#  mode    => '0600',
-#  content => "
-#    Host *
-#      IdentifyFile ~/.ssh/school
-#      PasswordAuthentication no
-#  ",
-#}
-
-file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^#?PasswordAuthentication',
+file { '~/.ssh/config':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "
+    Host 54.210.42.244
+      User ubuntu
+      IdentifyFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
-
-file_line { 'Declare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?IdentityFile',
-}
-
