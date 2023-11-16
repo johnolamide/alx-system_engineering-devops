10709f24cdab8f65c389883698bddc5d9dff3be5
Olamide Alokan <alokanolamide@gmail.com>
2023-11-16 20:18:18 +0100

    [ADD] 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# enable login by holberton user
+
+exec { 'increase-hard-file':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/^holberton soft/s/4/50000" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
